c11d53bc3c5443d628099e9c3b0d8a8d9008226d
Brad Hambrice <hambrice@uab.edu>
2018-01-20 08:42:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,10 +24,13 @@ attr_accessor :id, :name, :grade
    end
    
    def save
-    #if 
+    if self.id
+      self.update
+    else
     sql = "INSERT INTO students (name, grade) VALUES (?,?)"
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
     end
     
     def self.create(name, grade) 
@@ -44,6 +47,10 @@ attr_accessor :id, :name, :grade
 
     end
     
+    def self.find_by_name(name)
+      sql = "SELECT * FROM students WHERE name = ?"
+      self.new_from_db(DB[:conn].execute(sql, name).first)
+    end
     def update
       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
       DB[:conn].execute(sql, self.name,self.grade,self.id)
